516466df264a1be03744452f09be33e57d5fb8db
arvindh07 <arvindhganesh11@gmail.com>
2023-06-04 11:23:09 +0530

    added password hashing in signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,24 @@
+const User = require("../models/userModel")
+
 const userLogin = (req,res) => {
     res.json({
         msg:"login successfull"
     })
 }
 
-const userSignup = (req,res) => {
-    res.json({
-        msg:"signup successfull"
-    })
+const userSignup = async (req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await User.signup(email,password);
+        res.status(200).json({
+            email,
+            user
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
 }
 
 module.exports = {userLogin,userSignup};
