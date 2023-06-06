5115f482bd4269f67bcf059c2cdc4e8b68c93651
arvindh07 <arvindhganesh11@gmail.com>
2023-06-06 08:07:33 +0530

    added ui for signup and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Home from "./pages/Home";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/login" element={<Login />}/>
+        <Route exact path="/signup" element={<Signup />}/>
       </Routes>
     </BrowserRouter>
   );
